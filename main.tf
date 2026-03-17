e3d960420bc3e783f7f88abcac903d557d233872
Teja <tejag3171@gmail.com>
2026-03-18 01:05:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0c421724a94bba6d6"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "teja-key"
     tags = {
         Name = "DevOps"
     }
@@ -15,9 +15,9 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-05b5c04b3e4f5fc97", "subnet-078eaa4691fce41d5"]
+     subnets = ["subnet-068d74983d49cef2e", "subnet-017a974c159e5c50f"]
      listener {
-      instance_port     = 8000
+      instance_port     = 80
       instance_protocol = "http"
       lb_port           = 80
       lb_protocol       = "http"
